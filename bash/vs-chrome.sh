e54fe6ac483305529907553ff5b7b72388df95b8
Matt Jones <matt@serenity.home>
2020-08-12 04:30:10 +0100

    Download and unpack standalone chrome

--- bash/vs-chrome.sh
+++ bash/vs-chrome.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+#
+# https://storage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Linux_x64/
 
-CHROMEVERSION="549031"
+#CHROMEVERSION="549031"
+CHROMEVERSION="684285"
 GOOGLEHOST="https://storage.googleapis.com/chromium-browser-snapshots/Linux_x64/$CHROMEVERSION/chrome-linux.zip"
 
 
@@ -10,16 +13,20 @@ function install {
 	fi
 
 	if [ -d "$HOME/.vs-chrome/" ] ; then
+		echo "[+] Removing current install"
 		rm -rf $HOME/.vs-chrome
 	fi
 
-	wget $GOOGLEHOST -O /tmp/chrome-linux.zip
-	unzip /tmp/chrome-linux.zip -d ~/.vs-chrome
+	echo "[+] Downloading Chrome $CHROMEVERSION"
+	wget $GOOGLEHOST -O /tmp/chrome-linux.zip > /dev/null 2>&1
+	echo "[+] Installing to ~/.vs-chrome"
+	unzip /tmp/chrome-linux.zip -d ~/.vs-chrome > /dev/null 2>&1
+	echo "[+] Installation complete"
 }
 
 case $1 in
 	--install-chrome)
 		install ;;
 	*)
-		$HOME/.vs-chrome/chrome-linux/chrome "https://live.valueservices.uk" --no-sandbox
+		$HOME/.vs-chrome/chrome-linux/chrome "https://live.valueservices.uk" --no-sandbox --disable-setuid-sandbox
 esac
